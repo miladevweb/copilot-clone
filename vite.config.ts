bc2cf30ecf21d17585f829ac61fb69c2143ab7da
miladevweb <miladevweb@outlook.com>
2024-12-06 16:20:31 -0500

    docs: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })

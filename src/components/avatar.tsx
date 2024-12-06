42a207119c9c0b8196fc5c8c7a8efef7a38dfe46
miladevweb <miladevweb@outlook.com>
2024-12-06 17:28:18 -0500

    docs: create avatar component

---
+++ src/components/avatar.tsx
@@ -0,0 +1,11 @@
+export default function Avatar() {
+  return (
+    <nav>
+      <section className="h-16 flex justify-end items-end">
+        <picture className="size-10 mr-2">
+          <img className="rounded-full" src="./img1.jpg" alt="#" />
+        </picture>
+      </section>
+    </nav>
+  )
+}

062923b4d2b09748422fd3b3a1b53dfbcf1159ae
miladevweb <miladevweb@outlook.com>
2024-12-10 16:41:17 -0500

    docs: change responsive design

--- src/components/main-blur-card.tsx
+++ src/components/main-blur-card.tsx
@@ -7,19 +7,19 @@ type Props = {
 
 export default function MainBlurCard({ imgUrl, primaryLabel, secondaryLabel, buttonLabel }: Props) {
   return (
-    <div className="bg-[#483D4E] aspect-square w-full rounded-[12%]  overflow-clip grid grid-rows-[15%_85%] min-[360px]:grid-rows-[45%_55%] min-[490px]:grid-rows-[55%_45%] min-[532px]:grid-rows-[65%_35%]">
-      <picture>
+    <div className="aspect-square w-full rounded-[10%] overflow-clip relative flex flex-col justify-end">
+      <picture className="absolute top-0 left-0">
         <img
           alt="#"
           src={imgUrl}
         />
       </picture>
 
-      <div className="pb-6 px-7 flex flex-col justify-end gap-y-3 relative blur__class">
+      <div className="pb-6 px-7 bg-[#483D4E] flex flex-col justify-end gap-y-3 relative blur__class">
         <div>
           <h5>{secondaryLabel}</h5>
 
-          <h3 className="font-medium text-[1.55rem] leading-tight">{primaryLabel}</h3>
+          <h3 className="h3__class">{primaryLabel}</h3>
         </div>
 
         <button className="bg-[#E1BBA8] text-[#483D4E] block w-full h-12 rounded-[1.25rem]">{buttonLabel}</button>

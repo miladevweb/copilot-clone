df6d4eef1a6ce80ea8cdcf8565691d810411af95
miladevweb <miladevweb@outlook.com>
2024-12-10 17:22:29 -0500

    docs: add font color

--- src/components/main-blur-card.tsx
+++ src/components/main-blur-card.tsx
@@ -16,8 +16,8 @@ export default function MainBlurCard({ imgUrl, primaryLabel, secondaryLabel, but
       </picture>
 
       <div className="pb-6 px-7 bg-[#483D4E] flex flex-col justify-end gap-y-3 relative blur__class">
-        <div>
-          <h5>{secondaryLabel}</h5>
+        <div className="text-[#E1BBA8]">
+          <h5 className="opacity-80">{secondaryLabel}</h5>
 
           <h3 className="h3__class">{primaryLabel}</h3>
         </div>

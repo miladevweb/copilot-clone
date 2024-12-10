db942bbb4d6aae1c3cadb53e62cf70c337693cbb
miladevweb <miladevweb@outlook.com>
2024-12-10 17:45:58 -0500

    docs: change image paths

--- src/App.tsx
+++ src/App.tsx
@@ -8,81 +8,54 @@ export default function App() {
       <main>
         <Avatar />
 
-        <div className='flex justify-center'>
+        <div className="flex justify-center">
           <section className="px-6 pt-8 max-w-[816px]">
             <h1 className="header">Mila, it&apos;s great to see you</h1>
 
             <CardSection
-              imgUrl1="/img3.jpg"
-              imgUrl2="/img4.jpg"
+              imgUrl1="/img2.jfif"
+              imgUrl2="/img3.jfif"
               description1="Share your favorite author to get a book"
               description2="Let's co-write a birthday letter"
             >
               <MainBlurCard
-                imgUrl="/img2.jfif"
+                imgUrl="/img1.jfif"
                 buttonLabel="Play Now"
                 secondaryLabel="Today's top stories on Copilot Daily"
                 primaryLabel="Minimum wage increases, and more"
               />
             </CardSection>
 
+            {/* Topics Section */}
+            <h2 className="h2__class">Topics I thought you'd enjoy</h2>
+
             <CardSection
-              imgUrl1="/img5.jpg"
-              imgUrl2="/img6.jpg"
+              imgUrl1="/img5.jfif"
+              imgUrl2="/img6.jfif"
               description1="Share your favorite author to get a book"
               description2="Let's co-write a birthday letter"
             >
               <MainCard
-                imgUrl="/img7.jfif"
+                imgUrl="/img4.jfif"
+                description="Decompress from your day"
+              />
+            </CardSection>
+
+            {/* Fresh Section */}
+            <h2 className="h2__class">Something fresh</h2>
+
+            <CardSection
+              imgUrl1="/img8.jfif"
+              imgUrl2="/img9.jfif"
+              description1="Share your favorite author to get a book"
+              description2="Let's co-write a birthday letter"
+            >
+              <MainCard
+                imgUrl="/img7.png"
                 description="Decompress from your day"
               />
             </CardSection>
           </section>
-        </div>
-
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa recusandae error dignissimos ex voluptatum excepturi ipsum quos debitis, molestias eius beatae, soluta, vel consectetur.
-          Voluptates magnam cumque hic ratione necessitatibus iusto accusantium ea repudiandae, culpa, dolorem nostrum. Enim, odio. Iusto officiis praesentium necessitatibus, nobis sed blanditiis
-          nesciunt quia eveniet incidunt aliquid distinctio aut repellendus voluptates provident possimus, enim quae iste velit? Eligendi aspernatur veritatis suscipit, voluptas nemo vel obcaecati
-          reiciendis! Vel asperiores rerum ipsam, officiis sapiente natus et nulla magni quisquam sequi tempora odit, quibusdam iure at molestiae deleniti est iste minima illum cupiditate id.
-          Accusantium consequatur ea qui ipsam a! Ut id, autem quam ab voluptatum aliquid cupiditate quia, fugit omnis aut nulla nemo possimus deleniti, voluptates corrupti commodi ipsam nisi repellat
-          molestias! Debitis est totam sunt accusantium quidem hic, ad voluptates molestias molestiae modi. Culpa velit accusantium quisquam totam amet animi veniam? Tempora accusantium quis nulla
-          laboriosam illo ratione esse possimus voluptates, corrupti consectetur deserunt quisquam sapiente consequuntur! Praesentium officia aut ipsam neque fugit? Nemo deleniti voluptatem facilis
-          voluptates facere voluptate, debitis aliquam maxime eos doloremque consectetur similique optio. Aspernatur expedita quisquam asperiores eaque beatae iure eius doloremque, suscipit dolore,
-          assumenda voluptatibus? Ipsam neque est amet reprehenderit aliquid a nulla molestias aperiam perferendis quas fugiat nisi, dolor sint pariatur ex placeat ut? Doloribus obcaecati voluptate
-          accusamus minima consequatur perspiciatis suscipit libero, illum quos exercitationem sit corporis assumenda dolorem nemo at amet delectus facilis itaque quo maiores. Illum suscipit quae
-          pariatur, quaerat consequuntur corporis cupiditate illo tempore temporibus ab sed itaque doloremque eligendi nobis sapiente, nisi quia minima molestias laboriosam, consequatur dolor.
-          Voluptate itaque a molestias dolorem reiciendis corporis sequi tempore saepe rerum iste quis aliquid aut assumenda minus temporibus, maiores quos delectus! Maiores molestias aut similique
-          soluta, facere possimus deleniti sapiente illum quibusdam, quaerat, praesentium sequi! Repellendus, ullam? Consequatur quidem tempore assumenda voluptate sapiente, similique atque maiores,
-          voluptatibus quibusdam debitis rerum quas, nesciunt sed ab fugiat odio doloribus quam? Distinctio accusantium sequi beatae quaerat est rerum quibusdam tempora dolorem magnam quas impedit
-          deleniti neque sint fugit reiciendis, maxime aut. Perferendis atque nihil odit voluptate suscipit sed quo temporibus placeat aspernatur laborum beatae explicabo officia odio cupiditate non
-          vero obcaecati nemo recusandae, similique asperiores inventore! Impedit harum perspiciatis maxime sit blanditiis? Aperiam odio impedit nulla totam reiciendis dolorem, laboriosam maxime
-          adipisci veritatis est quibusdam a atque facere aut exercitationem optio vero explicabo commodi? Saepe natus labore aut ullam ipsa fuga rerum beatae magnam, at sequi assumenda, officia vitae
-          laudantium id! Quam ea voluptas dolorem eius assumenda nulla maiores voluptatem sed labore odio nam, quaerat inventore nesciunt. Sit, repellendus quia? Vero corporis incidunt saepe obcaecati
-          fuga ab eveniet! Consectetur culpa eum recusandae blanditiis repellendus quod porro numquam, cum pariatur odio perferendis necessitatibus in doloremque illum, nam tenetur repellat, eaque
-          corporis error odit velit ducimus aliquam officiis accusamus. Placeat vitae totam vel sed ipsam. Praesentium quam quisquam totam quasi maxime a cum? Saepe est necessitatibus facilis
-          consectetur. Maiores eum repellendus suscipit commodi labore qui aliquid laborum incidunt nam quasi vero nulla magnam quod voluptatem asperiores eveniet esse dignissimos ut vitae recusandae
-          necessitatibus nostrum fugiat, voluptas voluptate. Culpa molestias quam a maiores, tempora commodi ducimus fugit. Dolores commodi modi possimus quam enim recusandae unde quisquam maiores,
-          quis, non aliquam consectetur nisi delectus nihil? Vero maiores ad consequuntur odit adipisci facilis amet pariatur officiis quisquam hic dolorum provident corrupti, cumque excepturi libero
-          eveniet ab! Eos molestiae tenetur dolores consectetur pariatur corporis laborum fugit, consequuntur aspernatur officiis blanditiis, beatae numquam neque, quam repudiandae in sint commodi?
-          Eum hic illo soluta laudantium incidunt. Tempora repudiandae numquam autem, fugiat rem praesentium, odio optio commodi dicta nostrum vitae eum quisquam aperiam voluptate veniam? Voluptatem,
-          distinctio ex temporibus natus inventore, perspiciatis ducimus quas officiis exercitationem eos, vel aliquam enim fuga illum! Commodi dolorum fuga libero, necessitatibus iure ea, alias
-          inventore minima porro autem ad earum? Obcaecati, dolores aspernatur. Molestiae ipsam repellendus tempora excepturi repellat, fugiat laborum, assumenda blanditiis commodi, omnis unde quis
-          eveniet aspernatur quibusdam facere minima consectetur nemo dolores natus alias labore. Voluptatem tenetur dolores sed sint praesentium hic iusto, illum corporis, corrupti cumque at nulla
-          nemo deserunt, excepturi reiciendis ab quisquam alias necessitatibus error temporibus architecto consequuntur odit assumenda? Aliquam ad illum voluptates dolore excepturi hic quos quia id
-          amet, tempore repellendus, provident harum quidem eum? Assumenda quisquam aliquid porro fugit cumque provident necessitatibus velit! Placeat sed ullam possimus aliquam culpa magni
-          necessitatibus veniam inventore tempora enim. Sunt eos similique dignissimos facilis vel nihil quidem voluptatum. Sequi quia non, laboriosam odit fugiat reiciendis esse id aliquid voluptates
-          blanditiis consequatur alias impedit itaque quasi ad sint aperiam, facere hic recusandae! Adipisci ut nulla quasi fuga hic commodi, temporibus, quis consequuntur eveniet, totam dolorum vero
-          explicabo natus harum perspiciatis reiciendis reprehenderit obcaecati? Quod repellendus molestias enim totam modi quidem assumenda repellat voluptas commodi sunt, quos deserunt et
-          reprehenderit voluptatibus aliquid quo mollitia tenetur! Tenetur, eum ipsa nulla, ipsam reprehenderit, distinctio sint libero saepe repellat nobis sunt voluptatum beatae ipsum fugiat quis
-          labore cupiditate? Error rerum voluptatibus aliquid similique ducimus molestias repellat repellendus eligendi, explicabo dolor possimus quis? Sint temporibus quisquam, reprehenderit ratione
-          quam deserunt adipisci. Accusamus nobis libero, natus quisquam eum eveniet aliquid reprehenderit minus earum ullam delectus dolorem in. Quas quod vero ipsam repellat temporibus accusamus
-          necessitatibus, repudiandae id cupiditate nostrum, error quasi magnam harum fugiat, quis unde eum impedit at laboriosam labore ipsa doloribus. Impedit, omnis esse modi placeat rem sint ipsa
-          vero iste dolores atque animi soluta quidem sit nobis quisquam consequatur voluptate! Sequi magni explicabo atque porro ab totam fugit dolor aut quia laboriosam, excepturi animi, assumenda
-          nostrum quo sit debitis eaque eum corrupti quod. Officiis beatae ea similique, reiciendis iusto doloremque voluptates ipsam id praesentium perferendis natus quod maiores culpa minima
-          consequatur eaque adipisci, dolorem sit libero, consequuntur minus. Adipisci possimus facere eos qui quae repellat at veniam, voluptatum ducimus. Eos voluptatem suscipit a dignissimos ut
-          laborum deserunt labore reprehenderit quae quidem facilis nisi excepturi porro repudiandae ea aliquid doloribus, ex accusantium ipsam dolore obcaecati laudantium! Nisi reiciendis eveniet qui
-          ratione ullam nam.
         </div>
       </main>
     </>

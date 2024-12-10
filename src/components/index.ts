a32a5b441036547006d56043797dfcf60d3274d5
miladevweb <miladevweb@outlook.com>
2024-12-10 16:40:40 -0500

    docs: add main card

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Avatar from './avatar'
 import MiniCard from './mini-card'
 import CardSection from './card-section'
 import MainBlurCard from './main-blur-card'
+import MainCard from './main-card'
 
-export { Avatar, MiniCard, CardSection, MainBlurCard }
+export { Avatar, MiniCard, CardSection, MainBlurCard, MainCard }

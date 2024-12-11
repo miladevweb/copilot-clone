0b7e228789dcf3c8f8361cafdbbb92aff84bd3ac
miladevweb <miladevweb@outlook.com>
2024-12-11 10:47:50 -0500

    docs: change imgUrl

--- src/components/card-section.tsx
+++ src/components/card-section.tsx
@@ -1,14 +1,14 @@
 import { MiniCard } from './index'
 
 type Props = {
-  imgUrl1: string
-  imgUrl2: string
+  imgName1: string
+  imgName2: string
   description1: string
   description2: string
   children: React.ReactNode
 }
 
-export default function CardSection({ imgUrl1, imgUrl2, description1, description2, children }: Props) {
+export default function CardSection({ imgName1, imgName2, description1, description2, children }: Props) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 py-10">
       {/* Main Card */}
@@ -16,12 +16,12 @@ export default function CardSection({ imgUrl1, imgUrl2, description1, descriptio
 
       <div className="grid grid-rows-2 gap-y-5 h-80 sm:h-full">
         <MiniCard
-          imageUrl={imgUrl1}
+          imageName={imgName1}
           description={description1}
         />
 
         <MiniCard
-          imageUrl={imgUrl2}
+          imageName={imgName2}
           description={description2}
         />
       </div>

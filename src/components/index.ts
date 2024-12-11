bf92366d362307fd43fa0a6f85db95c1c8ca5c68
miladevweb <miladevweb@outlook.com>
2024-12-11 09:31:51 -0500

    docs: modify input import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-import Input from './input'
 import Avatar from './avatar'
 import MainCard from './main-card'
 import MiniCard from './mini-card'
 import CardSection from './card-section'
+import InputSection from './input-section'
 import MainBlurCard from './main-blur-card'
 
-export { Avatar, MiniCard, CardSection, MainBlurCard, MainCard, Input }
+export { Avatar, MiniCard, CardSection, MainBlurCard, MainCard, InputSection }

1416a8bd6df154296009b7063533e0bf93f7033f
miladevweb <miladevweb@outlook.com>
2024-12-11 10:08:31 -0500

    docs: add responsive bottom distance

--- src/components/input-section.tsx
+++ src/components/input-section.tsx
@@ -2,7 +2,7 @@ import { History, Microphone, Plus } from './svg'
 
 export default function InputSection() {
   return (
-    <div className="bg-[#1a223a] w-[90%] md:w-[700px] left-1/2  fixed bottom-10 py-2 px-4 outline outline-1 outline-white/20 rounded-full flex items-center gap-x-4 shadow-2xl input__class">
+    <div className="bg-[#1a223a] w-[90%] md:w-[700px] left-1/2  fixed sm:bottom-10 bottom-5 py-2 px-4 outline outline-1 outline-white/20 rounded-full flex items-center gap-x-4 shadow-2xl input__class">
       <div className="flex items-center gap-4">
         <History />
         <Plus />

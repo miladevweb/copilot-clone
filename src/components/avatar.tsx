73685487e1c7892993d7241d730803ec66bfd4f9
miladevweb <miladevweb@outlook.com>
2024-12-11 10:00:39 -0500

    docs: add cursor pointer

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,7 +2,7 @@ export default function Avatar() {
   return (
     <nav>
       <section className="h-16 flex justify-end items-end">
-        <picture className="size-10 mr-2">
+        <picture className="size-10 mr-2 cursor-pointer">
           <img
             alt="#"
             src="./avatar.jpg"
